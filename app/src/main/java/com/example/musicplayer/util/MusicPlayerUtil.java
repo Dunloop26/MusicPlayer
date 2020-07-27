0927769747c66275bbcc1ac8c885a1ad6b6b93aa
Wilzhar <wilmar2545@gmail.com>
2020-07-27 08:43:22 -0500

    UPDATE: Se añadieron los metodos para las conversiones de milisegundos a segundos o minutos

--- app/src/main/java/com/example/musicplayer/util/MusicPlayerUtil.java
+++ app/src/main/java/com/example/musicplayer/util/MusicPlayerUtil.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Handler;
 import android.util.TypedValue;
 
+import java.util.concurrent.TimeUnit;
+
 public class MusicPlayerUtil
 {
 	public static int dpToPx(float dp, Context context) {
@@ -12,5 +14,22 @@ public class MusicPlayerUtil
 
 	public static int spToPx(float sp, Context context) {
 		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.getResources().getDisplayMetrics());
+	}
+
+	public static double getMinutesFromMilliseconds(int milliseconds)
+	{
+		return (double) TimeUnit.MILLISECONDS.toMinutes(milliseconds);
+	}
+	public static int getSecondsFromMilliseconds(int milliseconds)
+	{
+		double minutes = getMinutesFromMilliseconds(milliseconds);
+		return (int) TimeUnit.MILLISECONDS.toSeconds(milliseconds) - (int)(minutes * 60);
+	}
+
+	public static String getFormatTimeFromMilliseconds(int milliseconds)
+	{
+		double minutes = getMinutesFromMilliseconds(milliseconds);
+		int seconds = getSecondsFromMilliseconds(milliseconds);
+		return String.format("%d:%02d",(int) Math.floor(minutes), seconds);
 	}
 }
