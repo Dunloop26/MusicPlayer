1184ab49c6144a251cd4b63d1da6d6f979f9eb7d
Wilzhar <wilmar2545@gmail.com>
2020-07-28 23:00:16 -0500

    ADD: Se creo una clase PlayList.java para la reproduccion de canciones de forma secuencial.

---
+++ app/src/main/java/com/example/musicplayer/PlayList.java
@@ -0,0 +1,55 @@
+package com.example.musicplayer;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class PlayList {
+	private ArrayList<File> _songs;
+	private String _name;
+	private Date _creationDate;
+	private int _totalSongs;
+	private int _indexCurrentSong;
+
+
+	public PlayList()
+	{
+		_indexCurrentSong = 0;
+		_songs = new ArrayList<>();
+	}
+
+
+	public void addSong(File song)
+	{
+		_songs.add(song);
+	}
+
+	public void setIndexCurrentSong(int indexCurrentSong) {
+		_indexCurrentSong = indexCurrentSong;
+	}
+
+	public void increaseSongIndex()
+	{
+		_indexCurrentSong++;
+		if(_indexCurrentSong >= _songs.size())
+		{
+			_indexCurrentSong = 0;
+		}
+	}
+
+	public File getCurrentSong()
+	{
+		return _songs.get(_indexCurrentSong);
+	}
+
+	public int indexOf(File songFile)
+	{
+		return _songs.indexOf(songFile);
+	}
+
+	public void setIndexFromSong(File songFile)
+	{
+		_indexCurrentSong = indexOf(songFile);
+
+	}
+}
