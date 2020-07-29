254a9cafc53be6bb90a94c9fffe08580eacea930
Wilzhar <wilmar2545@gmail.com>
2020-07-28 23:00:44 -0500

    UPDATE: Se borraron imports innecesarios

--- app/src/main/java/com/example/musicplayer/views/SongFileView.java
+++ app/src/main/java/com/example/musicplayer/views/SongFileView.java
@@ -13,7 +13,6 @@ import android.graphics.Rect;
 import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 
