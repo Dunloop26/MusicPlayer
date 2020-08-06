e142d68cbf76df74d419ad6ccf22211c4bace0e0
Wilzhar <wilmar2545@gmail.com>
2020-08-05 19:07:54 -0500

    UPDATE: Se cambio el codigo de generara las custom views con el SongFileView por un recyclerView, de esta manera queda el codigo mas limpio

--- app/src/main/java/com/example/musicplayer/activities/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/activities/MainActivity.java
@@ -1,28 +1,32 @@
 package com.example.musicplayer.activities;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import com.example.musicplayer.FileSearcher;
 import com.example.musicplayer.MP3Metadata;
 import com.example.musicplayer.PlayList;
+import com.example.musicplayer.SongViewAdapter;
 import com.example.musicplayer.TouchGestureSolver;
 import com.example.musicplayer.util.MetaDataWrapperUtil;
 import com.example.musicplayer.MusicApplication;
@@ -32,206 +36,102 @@ import com.example.musicplayer.util.MusicPlayerUtil;
 import com.example.musicplayer.views.SongFileView;
 
 import java.io.File;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SongViewAdapter.OnSongListener {
 
-    private ViewGroup _fileViewContainer;
+	public static final String TAG = "MainActivity";
     private SongWrapper _songWrapper;
 	private Intent _songDetailsIntent;
+	private Intent _songOptionsIntent;
 	private PlayList _playList;
-	private Handler _addSongViewsHandler;
+	private ArrayList<File> _files;
+
+	private RecyclerView recyclerView;
+	private SongViewAdapter mAdapter;
+	private RecyclerView.LayoutManager layoutManager;
 
     @SuppressLint("HandlerLeak")
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-		_fileViewContainer = findViewById(R.id.songListContainer);
+        Log.d("Hola", "inicia");
 
         if (Build.VERSION.SDK_INT >= 23)
             requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
 
 		if(_songDetailsIntent == null)
 			_songDetailsIntent = new Intent(this, SongDetailsActivity.class);
+		if(_songOptionsIntent == null)
+			_songOptionsIntent = new Intent(this, SongOptionsActivity.class);
 
         _songWrapper = ((MusicApplication) getApplication()).getSongWrapper();
         _playList = ((MusicApplication) getApplication()).getPlayList();
-
-		_addSongViewsHandler = new Handler()
-		{
-			@SuppressLint("HandlerLeak")
-			@Override
-			public void handleMessage(@NonNull Message msg) {
-				SongFileView[] songFileViews = (SongFileView[]) msg.obj;
-				for(int indexSongView = 0; indexSongView < songFileViews.length; indexSongView++)
-				{
-					_fileViewContainer.addView(songFileViews[indexSongView]);
-				}
-			}
-		};
-
-        _songWrapper.addOnSongChangeActionListener(new SongWrapper.OnSongChangeActionListener() {
+		_songWrapper.addOnSongChangeActionListener(new SongWrapper.OnSongChangeActionListener() {
 			@Override
 			public void onSongChangeAction(SongWrapper sw, File file) {
 				onPlayListSongChangeAction(sw, file);
 			}
 		});
+
+		recyclerView = (RecyclerView) findViewById(R.id.songListContainer);
+
+		recyclerView.setHasFixedSize(true);
+
+		layoutManager = new LinearLayoutManager(this);
+		_files = executeFileSearcher();
+		_playList.setSongList(_files);
+
+
+
+		MP3Metadata[] metadataSongs = new MP3Metadata[_files.size()];
+
+		for(int i = 0; i < metadataSongs.length; i++) {
+			metadataSongs[i] = MetaDataWrapperUtil.MP3FromFile(_files.get(i));
+		}
+
+		recyclerView.setLayoutManager(layoutManager);
+
+		mAdapter = new SongViewAdapter(metadataSongs, this);
+		recyclerView.setAdapter(mAdapter);
     }
 
 	@Override
 	protected void onStart() {
-		Runnable loadSongViewsRunnable = new Runnable() {
-			@Override
-			public void run() {
-				SongFileView[] songFileViews = createFileView(executeFileSearcher());
-				Message message = new Message();
-				message.obj = songFileViews;
-				_addSongViewsHandler.sendMessage(message);
-			}
-		};
 
-		Thread mythread = new Thread(loadSongViewsRunnable);
-		mythread.start();
 		super.onStart();
 	}
 
-	private File[] executeFileSearcher() {
+	private ArrayList<File> executeFileSearcher() {
         FileSearcher fileSearcher = new FileSearcher(".mp3", this);
         fileSearcher.findFilesOnPath(fileSearcher.getRootPath());
-        File[] files = fileSearcher.getFiles();
+        ArrayList<File> files = fileSearcher.getFiles();
         fileSearcher.printFileUtil(files);
         return files;
     }
 
-    int indexSong;
-    @SuppressLint("ClickableViewAccessibility")
-    private SongFileView[] createFileView(File[] files) {
-		int length = files.length;
-		SongFileView[] songFileViews = new SongFileView[length];
-		for (int fileIndex = 0; fileIndex < length; fileIndex++) {
-
-        	indexSong = fileIndex;
-
-            File currentFile = files[fileIndex];
-			_playList.addSong(currentFile);
-
-            if (currentFile == null) continue;
-            if (!currentFile.canRead()) continue;
-
-            final SongFileView view = new SongFileView(this);
-			setupViewData(this, currentFile, view);
-			view.setOnTouchListener(new TouchGestureSolver() {
-				private boolean moreOptions = false;
-				private boolean animation = false;
-
-				@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-				@Override
-				public boolean onTouch(View v, MotionEvent event) {
-					int eventAction = event.getAction();
-
-					switch (eventAction) {
-						case MotionEvent.ACTION_DOWN:
-							int x = (int) event.getX();
-							int y = (int) event.getY();
-							if (view.getRectMoreOptions().contains(x, y))
-							{
-								moreOptions = true;
-								animation = false;
-							}
-							else
-							{
-								animation = true;
-								moreOptions = false;
-								view.animateHoldTouched();
-							}
-							break;
-
-						case MotionEvent.ACTION_MOVE:
-
-							break;
-
-						case MotionEvent.ACTION_CANCEL:
-							if(animation)
-							{
-								view.animateReleaseTouched();
-							}
-							animation = false;
-							break;
-
-						case MotionEvent.ACTION_UP:
-							if(animation)
-								view.animateReleaseTouched();
-							break;
-					}
-
-					return super.onTouch(v, event);
-				}
-
-				@Override
-				public void onLongClick() {
-					Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-					// Vibrate for 30 milliseconds
-					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-						v.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.EFFECT_TICK));
-					} else {
-						//deprecated in API 26 or higher
-						v.vibrate(30);
-					}
-					Toast.makeText(MainActivity.this, "This is my Toast message!",
-							Toast.LENGTH_LONG).show();
-					if(animation)
-						view.animateReleaseTouched();
-					animation = false;
-				}
-
-				@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-				@Override
-				public void onSingleClick(View v) {
-
-					if (moreOptions) {
-						Intent intent = new Intent(MainActivity.this, SongOptionsActivity.class);
-						startActivity(intent);
-					} else {
-						songClickListener((SongFileView) v);
-					}
-				}
-
-			});
-
-            LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(_fileViewContainer.getWidth(), MusicPlayerUtil.dpToPx(70, this));
-            view.setLayoutParams(layout);
-            songFileViews[fileIndex] = view;
-        }
-		return  songFileViews;
-	}
-
-	private void setupViewData(Context context, File songFile, SongFileView view)
-	{
-		MP3Metadata metadata = MetaDataWrapperUtil.MP3FromFile(songFile);
-
-		view.setNameTextSize(MusicPlayerUtil.spToPx(17, context));
-		view.setArtistAlbumNameTextSize(MusicPlayerUtil.spToPx(15, context));
-
-		view.setFileDisplayAlbumName(metadata.albumName);
-		view.setFileDisplayArtistName(metadata.artistName);
-		view.setImage(metadata.image);
-		view.setReferenceFile(songFile);
-
-		String title = metadata.title;
-		view.setFileDisplayName(
-				title.equals(MetaDataWrapperUtil.UNKNOWN_TITLE)
-						? songFile.getName()
-						: title);
-
-	}
-
 	@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-	private void songClickListener(SongFileView songFileView) {
+	@Override
+	public void onClickSong(int position) {
+    	onPlaySong(_files.get(position));
+	}
+
+	@Override
+	public void onLongClickSong(int position) {
+		Toast.makeText(this, "This is a message", Toast.LENGTH_LONG).show();
+    }
+
+	@Override
+	public void onClickSongOptions(int position) {
+    	startActivity(_songOptionsIntent);
+	}
+	@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+	private void onPlaySong(File file) {
 		if (_songWrapper == null) return;
-		_songWrapper.play(songFileView.getReferenceFile());
-		_playList.setIndexFromSong(songFileView.getReferenceFile());
-		MP3Metadata metadata = MetaDataWrapperUtil.MP3FromFile(songFileView.getReferenceFile());
+		_songWrapper.play(file);
+		_playList.setIndexFromSong(file);
+		MP3Metadata metadata = MetaDataWrapperUtil.MP3FromFile(file);
 
 
 		Bundle options = new Bundle();
