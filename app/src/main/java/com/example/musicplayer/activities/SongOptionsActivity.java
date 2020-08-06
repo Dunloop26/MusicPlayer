71c94c6a32ef44394aac4a964320b155c50fd2c9
Wilzhar <wilmar2545@gmail.com>
2020-08-06 10:07:35 -0500

    UPDATE: Se añadieron los listeners de las TextView en el SongOptionsActivity

--- app/src/main/java/com/example/musicplayer/activities/SongOptionsActivity.java
+++ app/src/main/java/com/example/musicplayer/activities/SongOptionsActivity.java
@@ -8,18 +8,67 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.musicplayer.R;
 
 public class SongOptionsActivity extends AppCompatActivity {
 
+    private static final String TAG = "SongOptionsActivity";
+
+    private TextView txtName;
+    private TextView txtPlayLater;
+    private TextView txtAddTo;
+    private TextView txtDelete;
+    private TextView txtShare;
+    private TextView txtFavorite;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_options);
+        txtName = (TextView) findViewById(R.id.txtName);
+        txtPlayLater = (TextView) findViewById(R.id.txtPlayLater);
+        txtAddTo = (TextView) findViewById(R.id.txtAddTo);
+        txtDelete = (TextView) findViewById(R.id.txtDelete);
+        txtShare = (TextView) findViewById(R.id.txtShare);
+        txtFavorite = (TextView) findViewById(R.id.txtFavorite);
 
+        txtPlayLater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Play later");
+            }
+        });
 
+        txtAddTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Add to");
+            }
+        });
 
+        txtDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Delete");
+            }
+        });
+
+        txtShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Share");
+            }
+        });
+
+        txtFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Favorite");
+            }
+        });
     }
 
     @Override
